1dbfa93f1c5b53202e2f0584f8c6259aa4ae8f71
ros-e <176725295+ros-e@users.noreply.github.com>
2026-03-03 11:54:40 -0600

    feat: error message and basic socket creation

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define PORT 3000
-
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        // change this to an error message
+        printf("no port dumbass");
         exit(1);
     }
     printf("HTTP Server Started on %s\n", argv[1]);
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(1);
+    }
+
 }
